4fa6c15edd086d8697c5c3f994bf63012b952f9e
Matt Hung <matthung@cherricorp.com>
2020-06-15 14:31:46 +0800

    config Socket Server

--- src/main/java/com/abc/demo/App.java
+++ src/main/java/com/abc/demo/App.java
@@ -1,13 +1,44 @@
 package com.abc.demo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class App {
+
+    private static final int PORT = 5000;
+
+    public static void main(String[] args) {
+
+        new SocketServer(PORT);
+    }
+
+    static class SocketServer {
+
+        SocketServer(int port) {
+            System.out.println("Socket Server starting...");
+            try (
+                    ServerSocket serverSocket = new ServerSocket(port);
+                    Socket clientSocket = serverSocket.accept();
+                    DataInputStream in = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+            ) {
+                System.out.println("Socket Server started... port=" + PORT);
+
+                String line = "";
+                while (! "exit".equals(line)) {
+                    try {
+                        line = in.readUTF();
+                        System.out.println(line);
+
+                    } catch (IOException i) {
+                        System.out.println(i);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
